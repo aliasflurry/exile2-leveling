e98b0af6f0596137e0c996488581c397696cdb07
Hoang Luong <48219316+aliasflurry@users.noreply.github.com>
2025-04-16 16:05:23 -0400

    Deploy gh-page

--- node_modules/web/dist/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/exile2-leveling/sw.js', { scope: '/exile2-leveling/' })})}
--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/exile-leveling/sw.js', { scope: '/exile-leveling/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/exile2-leveling/sw.js', { scope: '/exile2-leveling/' })})}
